8e71c391b253db180305d3898073dcffac6a85fa
pranav_padmasali <pranavpadmasali@gmail.com>
2016-07-31 15:18:28 +0530

    Manish added with chu

--- addition.c
+++ addition.c
@@ -3,7 +3,7 @@
 int main() 
 {
 	int a,b;	
-	printf("\nEnter two numbers \n");
+	printf("\nEnter two numbers Manish is chu\n");
 	scanf("%d%d",&a,&b);
 	printf("\nThe sum is  = %d \n",a+b);
 	return 0;	
